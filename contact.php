24ac524313f7895f1c4a011f7f9faa26c3359b5e
LuggageLux <143216703+LuggageLux@users.noreply.github.com>
2023-08-27 16:08:46 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php
+    $visitor_email = $POST['email'];
+
+    $email_from = "luggagelux@gmail.com";
+    $email_subject = "New Form Submission";
+    $email_body ="User Email: $visitor_email"
+
+    
+    $to = "luggagelux@gmail.com"
+    $headers = "From: $email_from \r\n";
+    $headers = "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: index.html")

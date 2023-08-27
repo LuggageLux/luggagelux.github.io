d979f732c8933e4e65efd5ebe18f48153f3157f1
LuggageLux <143216703+LuggageLux@users.noreply.github.com>
2023-08-27 16:18:56 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,15 +1,14 @@
 <?php
     $visitor_email = $_POST['email'];
 
-    $email_from = "luggagelux@gmail.com";
+    $email_from = "Luggagelux@gmail.com";
     $email_subject = "New Form Submission";
-    $email_body ="User Email: $visitor_email";
-
+    $email_body = "Email: $visitor_email";
     
     $to = "luggagelux@gmail.com";
     $headers = "From: $email_from \r\n";
-    $headers = "Reply-To: $visitor_email \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
 
-    mail($to,$email_subject,$email_body,$headers);
+    mail($to, $email_subject, $email_body, $headers);
     header("Location: index.html");
 ?>
